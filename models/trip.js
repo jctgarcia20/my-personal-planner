d1be3251b45358a08ccd6ba391cbf26b2d651bee
Jordan-Christopher Garcia <jctgarcia20@gmail.com>
2022-04-23 19:51:46 -0700

    Add log in & log out to the nav bar

--- models/trip.js
+++ models/trip.js
@@ -3,7 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-  
+  // _id: ,
+  // userFavoriting: ,
+  location: {
+    type: String
+  },
+  budget:  {
+    type: Number
+  },
+  expenses: {
+    type: Number
+  },
+  Public: {
+    type: Boolean,
+    default: false
+  },
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Trip', tripSchema);
